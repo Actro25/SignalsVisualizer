af93121a1cbfd9c5bdbb7e4796c02dede5d465c4
Vergil <skiril502@gmail.com>
2026-07-11 14:44:12 +0300

    feat: was created first rout which returns 'Hello World!'

--- SignalsVisualizer/src/main.rs
+++ SignalsVisualizer/src/main.rs
@@ -1,5 +1,22 @@
+use axum::Router;
+use axum::response::Html;
+use axum::routing::{Route, get};
 
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let app = app();
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+
+    println!(
+        "Server is listening on {} .",
+        listener.local_addr().unwrap()
+    );
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+fn app() -> Router {
+    Router::new().route("/", get(|| async { Html("Hello World!") }))
 }
