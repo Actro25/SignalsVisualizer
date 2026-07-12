0867ee90003c41b20ac9173f62c7dfcc3e208c4d
Vergil <skiril502@gmail.com>
2026-07-12 15:06:40 +0300

    feat: added WS into project

--- SignalsVisualizer/src/handlers/index.rs
+++ SignalsVisualizer/src/handlers/index.rs
@@ -1,8 +1,28 @@
 use askama::Template;
+use axum::extract::ws::{Message, Utf8Bytes, WebSocket, WebSocketUpgrade};
 use axum::response::{Html, IntoResponse, Response};
 use crate::models::templates::{HomeTemplate};
 
 pub async fn home() -> Response{
     let html_string = HomeTemplate{}.render().unwrap();
     Html(html_string).into_response()
+}
+
+
+pub async fn ws_data_transfer(ws: WebSocketUpgrade) -> impl IntoResponse {
+    ws.on_upgrade(handler_socket)
+}
+
+async fn handler_socket(mut socket: WebSocket) {
+    let mut counter: u64 = 0;
+    loop {
+        counter += 1;
+        let transfer_value = counter.to_string();
+
+        if socket.send(Message::Text(Utf8Bytes::from(transfer_value))).await.is_err() {
+            break;
+        }
+
+        tokio::time::sleep(std::time::Duration::from_millis(200)).await;
+    }
 }
--- SignalsVisualizer/src/router.rs
+++ SignalsVisualizer/src/router.rs
@@ -1,7 +1,9 @@
 use axum::Router;
-use axum::routing::get;
-use crate::handlers::index::home;
+use axum::routing::{get, post};
+use crate::handlers::index::{home, ws_data_transfer};
 
 pub fn router() -> Router{
-    Router::new().route("/", get(home))
+    Router::new()
+        .route("/", get(home))
+        .route("/ws", get(ws_data_transfer))
 }
