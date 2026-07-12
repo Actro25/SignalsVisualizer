17035b370a7e7de1d16a853e38b065dc59f799d7
Vergil <skiril502@gmail.com>
2026-07-12 15:38:56 +0300

    feat: was added plot into HTML

--- SignalsVisualizer/src/router.rs
+++ SignalsVisualizer/src/router.rs
@@ -1,9 +1,10 @@
-use axum::Router;
-use axum::routing::{get, post};
+use axum::routing::{get, Router};
+use tower_http::services::ServeDir;
 use crate::handlers::index::{home, ws_data_transfer};
 
 pub fn router() -> Router{
     Router::new()
         .route("/", get(home))
         .route("/ws", get(ws_data_transfer))
+        .fallback_service(ServeDir::new("static"))
 }
---
+++ SignalsVisualizer/static/js/home.js
@@ -0,0 +1,52 @@
+const startButton = document.getElementById("ws_start");
+const stopButton = document.getElementById("ws_stop");
+const loadPlotButton = document.getElementById("load_plot");
+
+const showPlotDiv = document.getElementById("show");
+
+let ws = null;
+let uplot = null;
+
+startButton.addEventListener('click', function () {
+    if(ws){
+        console.log("Active.");
+        return;
+    }
+
+    ws = new WebSocket("/ws");
+
+    ws.onmessage = (event) => {
+        const value = parseInt(event.data);
+        console.log(value);
+    }
+
+    ws.onerror = (error) => {
+        console.log("Error WebSocket: ", error);
+    }
+})
+
+stopButton.addEventListener('click', function () {
+    ws.close();
+    ws = null;
+})
+
+loadPlotButton.addEventListener('click', function () {
+    const data = [
+        [0, 1, 2, 3, 4, 5],
+        [10, 15, 13, 17, 20, 18],
+    ];
+
+    const opts = {
+        width: 800,
+        height: 400,
+        series: [
+            {}, // конфіг для X-осі (зазвичай порожній)
+            {
+                label: "Значення",
+                stroke: "blue",
+                width: 2,
+            },
+        ],
+    };
+    uplot = new uPlot(opts, data, showPlotDiv);
+})
