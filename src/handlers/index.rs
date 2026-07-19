a47be79fad010b146f4d2b860a920945601558fa
Vergil <skiril502@gmail.com>
2026-07-19 17:25:09 +0300

    refactor: was redone code into two additional threads

--- src/handlers/index.rs
+++ src/handlers/index.rs
@@ -3,19 +3,20 @@ use crate::generator::point::Point;
 use crate::generator::signals::Signals;
 use crate::models::templates::HomeTemplate;
 use askama::Template;
+use atomic_float::AtomicF64;
 use axum::extract::State;
 use axum::extract::ws::{Message, WebSocket, WebSocketUpgrade};
 use axum::response::{Html, IntoResponse, Response};
+use futures_util::stream::{SplitSink, SplitStream};
 use futures_util::{SinkExt, StreamExt};
 use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
-use atomic_float::AtomicF64;
 use tokio::sync::broadcast::{Receiver, Sender};
 
 #[derive(serde::Deserialize)]
 struct Command {
     action: String,
-    value: f64
+    value: f64,
 }
 pub async fn home() -> Response {
     let html_string = HomeTemplate {}.render().unwrap();
@@ -36,7 +37,12 @@ pub async fn ws_data_transfer_handler(
     create_new_thread_with_signals(signal, prod);
 
     ws.on_upgrade(async move |socket| {
-        send_data_via_ws(socket, consumer, amplitude, frequency).await;
+        let (sender, receiver) = socket.split();
+        let rx_handle = create_new_thread_with_ws_receiver(receiver, amplitude, frequency);
+        let tx_handle = create_new_thread_with_ws_sender(sender, consumer);
+
+        let _ = tokio::join!(rx_handle, tx_handle);
+
         println!("Web Socket connection was closed.");
         working.store(false, Ordering::Relaxed);
     })
@@ -51,63 +57,72 @@ fn create_new_thread_with_signals(
     });
 }
 
-async fn send_data_via_ws(socket: WebSocket, mut cons: Receiver<Point>, mut amplitude: Arc<AtomicF64>,mut frequency: Arc<AtomicF64>) {
-    let (mut sender, mut receiver) = socket.split();
-
-    loop {
-        tokio::select! {
-             message = receiver.next() => {
-                match message {
-                    Some(Ok(Message::Text(msg))) => {
-                        if let Ok(command) = serde_json::from_str::<Command>(&msg){
+pub fn create_new_thread_with_ws_receiver(
+    mut receiver: SplitStream<WebSocket>,
+    amplitude: Arc<AtomicF64>,
+    frequency: Arc<AtomicF64>,
+) -> tokio::task::JoinHandle<()> {
+    tokio::spawn(async move {
+        while let Some(result) = receiver.next().await {
+            match result {
+                Ok(message) => match message {
+                    Message::Text(msg) => {
+                        if let Ok(command) = serde_json::from_str::<Command>(&msg) {
                             match command.action.as_str() {
                                 "change_amplitude" => {
                                     amplitude.store(command.value, Ordering::Relaxed);
-                                },
+                                }
                                 "change_frequency" => {
                                     frequency.store(command.value, Ordering::Relaxed);
-                                },
-                                _ => println!("Unseen command.")
+                                }
+                                _ => println!("Unseen command."),
                             }
                         }
-                    },
-                    Some(Ok(Message::Close(_))) | None => {
+                    }
+                    Message::Close(_) => {
                         println!("Client closed connection");
                         break;
                     }
-                    Some(Err(e)) => {
-                        println!("WS read error: {e}");
-                        break;
-                    }
                     _ => {}
-                }
-            }
-
-            point = cons.recv() => {
-                match point {
-                    Ok(point) => {
-                        let transfer_value: Point = Point {
-                            x: point.x,
-                            y: point.y,
-                        };
-                        match serde_json::to_string(&transfer_value) {
-                            Ok(json_string) => {
-                                if sender
-                                    .send(Message::Text(json_string.into()))
-                                    .await
-                                    .is_err()
-                                {
-                                    break;
-                                }
-                            }
-                            Err(e) => eprintln!("Error JSON serializing: {}", e),
-                        }
-                    }
-                    Err(e) => {
-                        println!("There is error in handler_socket: {}", e);
-                    }
+                },
+                Err(e) => {
+                    println!("WS read error: {e}");
+                    break;
                 }
             }
         }
-    }
+    })
+}
+
+pub fn create_new_thread_with_ws_sender(
+    mut sender: SplitSink<WebSocket, Message>,
+    mut cons: Receiver<Point>,
+) -> tokio::task::JoinHandle<()> {
+    tokio::spawn(async move {
+        loop {
+            match cons.recv().await {
+                Ok(point) => {
+                    let transfer_value: Point = Point {
+                        x: point.x,
+                        y: point.y,
+                    };
+                    match serde_json::to_string(&transfer_value) {
+                        Ok(json_string) => {
+                            if sender
+                                .send(Message::Text(json_string.into()))
+                                .await
+                                .is_err()
+                            {
+                                break;
+                            }
+                        }
+                        Err(e) => eprintln!("Error JSON serializing: {}", e),
+                    }
+                }
+                Err(e) => {
+                    println!("There is error in handler_socket: {}", e);
+                }
+            }
+        }
+    })
 }
